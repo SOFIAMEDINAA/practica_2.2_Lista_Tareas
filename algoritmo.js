fdde666440e3fc4a6613df0771b854bf11bcfb46
Sofia Medina <medinasofiaa1@gmail.com>
2023-08-23 12:19:21 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,19 @@
+const inputNuevaTarea = document.querySelector("#nueva-tarea");
+
+const CLAVE_TAREAS= "Tarea";
+
+let listaTareas= "lista vacia";
+listaTareas = [];
+
+function agregar() {
+    let nuevaTarea = inputNuevaTarea.value;
+    document.querySelector("ol").innerHTML += `
+    <li> ${nuevaTarea} </li>
+    `;
+    
+    
+    listaTareas.push (nuevaTarea);
+
+    localStorage.setItem(CLAVE_TAREAS , JSON.stringify(listaTareas));
+    
+}
